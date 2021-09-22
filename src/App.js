eec444391bcaa9331905db315e51135d335c039d
Renan Tokashiki <renantoka@icloud.com>
2021-09-22 14:05:49 -0400

    feat: created the base of the app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
+  const [query, setQuery] = useState("");
+
+  var url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`;
+
+  async function getRecipes() {
+    var result = await axios.get(url);
+    console.log(result.data);
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    getRecipes();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="app__searchForm" onSubmit={onSubmit}>
+        <input
+          className="app__input"
+          type="text"
+          placeholder="enter search"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <input
+          className="app__submit"
+          type="submit"
+          value="search"
+        />
+      </form>
     </div>
   );
 }
